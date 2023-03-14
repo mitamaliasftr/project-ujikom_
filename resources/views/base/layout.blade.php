6c050e1ba82fc17e9e69634f2cbf970d6e0e93ad
mitamaliasftr <mitamaliasftri@gmail.com>
2023-03-14 22:38:10 +0700

    templating

---
+++ resources/views/base/layout.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Purple Admin</title>
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="assets/vendors/mdi/css/materialdesignicons.min.css">
+    <link rel="stylesheet" href="assets/vendors/css/vendor.bundle.base.css">
+    <!-- endinject -->
+    <!-- Plugin css for this page -->
+    <!-- End plugin css for this page -->
+    <!-- inject:css -->
+    <!-- endinject -->
+    <!-- Layout styles -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    <!-- End layout styles -->
+    <link rel="shortcut icon" href="assets/images/favicon.ico" />
+  </head>
+  <body>
+    <div class="container-scroller">
+        <!-- partial:partials/_navbar.html -->
+      @include('base.navbar')
+      <!-- partial -->
+      <div class="container-fluid page-body-wrapper">
+        <!-- partial:partials/_sidebar.html -->
+        @include('base.sidebar')
+        <!-- partial -->
+        @yield('content')
+          <!-- content-wrapper ends -->
---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,16 @@
+@extends('base.layout')
+
+@push('styles')
+    
+@endpush
+
+@section('content')
+<div class="main-panel">
+    <div class="content-wrapper">
+        
+    </div>
+@endsection
+
+@push('scripts')
+    
+@endpush
